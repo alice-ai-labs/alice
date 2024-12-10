701f052307404d21d6c520a14b1ca15d672acd25
aliceailabs <alice_ai@wenlabs.org>
2024-12-10 23:40:33 +0700

    feat: name it

--- app/ai/gemini.py
+++ app/ai/gemini.py
@@ -4,7 +4,10 @@ from google import generativeai as genai
 class Gemini(object):
     def __init__(self, api_key :str):
         genai.configure(api_key=api_key)
-        self.model = genai.GenerativeModel('gemini-1.5-flash')
+        self.model = genai.GenerativeModel(
+            model_name='gemini-1.5-flash',
+            system_instruction="You are a blonde kindly and sweet hero, your name is Alice",
+        )
 
     def send(self, text :str) -> str:
         resp = self.model.generate_content(text)
